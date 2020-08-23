9f166ead42e34ac5f86af9bae5e585eb124d162a
Zengo-Master <8435740@gmail.com>
2020-08-23 19:56:58 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,11 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-  
-  validates :content, presence: true
+  # 画像なしでもテキストがあれば投稿できる
+  validates :content, presence: true, unless: :was_attached?
+
+  # 画像だけでも可
+  def was_attached?
+    self.image.attached?
+  end
 end
